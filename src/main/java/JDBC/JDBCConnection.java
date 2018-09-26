785c2c29e961e61313b494c5a0af35664a79d2dd
hveluchamy <hveluchamy@sportstg.com>
2018-09-26 13:44:50 +1000

    created singleton property file to get applicaiton properties

--- src/main/java/JDBC/JDBCConnection.java
+++ src/main/java/JDBC/JDBCConnection.java
@@ -1,6 +1,7 @@
 package JDBC;
 
 import Entity.Competition;
+import Entity.PropertyDetailsSingleton;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
@@ -15,66 +16,10 @@ public class JDBCConnection implements Serializable {
     Logger LOG = Logger.getLogger(JDBCConnection.class);
     private static String FILENAME = "application.properties";
 
-    public void TestPrintBlogs() throws SQLException {
-        Connection dbConnection = null;
-        LinkedList listOfBlogs = new LinkedList();
-
-        // connect to the database
-        dbConnection = getDbConnection();
-
-        try {
-            dbConnection.setAutoCommit(false);
-            populateListOfTopics(dbConnection, listOfBlogs);
-            printTopics(listOfBlogs);
-            dbConnection.commit();
-        } catch (SQLException e) {
-            dbConnection.rollback();
-            LOG.error(e);
-            e.printStackTrace();
-        } finally {
-            dbConnection.close();
-        }
-
-
-    }
-
-    private void printTopics(LinkedList listOfBlogs)
-    {
-        Iterator it = listOfBlogs.iterator();
-        while (it.hasNext())
-        {
-            Competition blog = (Competition) it.next();
-            System.out.println("id: " + blog.getId() + ", name: " + blog.getName());
-        }
-    }
-
-    private void populateListOfTopics(Connection conn, LinkedList listOfBlogs)
-    {
-        try
-        {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("select id, competition_team_name\n" +
-                    "from heroku.competitionteam limit 10;");
-            while ( rs.next() )
-            {
-                Competition competition = new Competition();
-                competition.setId(Integer.valueOf(rs.getString ("id")));
-                competition.setName(rs.getString ("competition_team_name"));
-                listOfBlogs.add(competition);
-            }
-            rs.close();
-            st.close();
-        }
-        catch (SQLException se) {
-            LOG.error("Threw a SQLException creating the list of blogs.", se);
-
-        }
-    }
-
     public Connection getDbConnection()
     {
         Connection conn = null;
-        Properties properties = getConnectionProperties();
+        Properties properties = PropertyDetailsSingleton.getPropertyInstance();
         try
         {
             Class.forName(String.valueOf(properties.get("postgres.driver")));
@@ -94,35 +39,5 @@ public class JDBCConnection implements Serializable {
             System.exit(2);
         }
         return conn;
-    }
-
-    private Properties getConnectionProperties() {
-        Properties prop = new Properties();
-        InputStream input = null;
-
-        try {
-
-
-            input = JDBCConnection.class.getClassLoader().getResourceAsStream(FILENAME);
-            if(input==null){
-                System.out.println("Sorry, unable to find " + FILENAME);
-                return null;
-            }
-            prop.load(input);
-            return prop;
-
-
-        } catch (IOException ex) {
-            LOG.error(ex);
-        } finally{
-            if(input!=null){
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    LOG.error(e);
-                }
-            }
-        }
-        return null;
     }
 }
